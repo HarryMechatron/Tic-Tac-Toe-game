87344f5adef73b68196409134fda467ba66e5083
Hari_Prasanth_SM <harrymechatron@gmail.com>
2020-08-13 17:51:11 +0530

    get players name added

--- game.py
+++ game.py
@@ -24,4 +24,17 @@ def print_intro():
 	print("The board with initial position is: ")
 	display_board([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
+def get_player_info():
+	'''
+	input    : -
+	output   : Player names
+	function : Get the two player names from user 
+	'''
+	player1 = input("Player 1: Enter your name: ")
+	print(f"Hi {player1}, your symbol is 'X'")
+	player2 = input("Player 2: Enter your name: ")
+	print(f"Hi {player2}, your symbol is 'O'")
+	return player1, player2
+
 print_intro()
+get_player_info()
